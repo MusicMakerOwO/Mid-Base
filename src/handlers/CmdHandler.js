ea4f4f8056003cce1573be75582ea1dda761cc07
Music Maker <joshuanejdl@gmail.com>
2024-10-24 01:38:42 -0500

    Fixed command handler

--- src/handlers/CmdHandler.js
+++ src/handlers/CmdHandler.js
@@ -39,27 +39,32 @@ client.on('messageCreate', async message => {;
  });
  }
 
-for (const folder of commandFolders) {
-	// Grab all the command files from the commands directory you created earlier
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			commands.push(command.data.toJSON());
-		} else {
-			console.warn(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+
+module.exports = async (client) => {
+
+	client.commands = new Collection();
+
+	for (const folder of commandFolders) {
+		// Grab all the command files from the commands directory you created earlier
+		const commandsPath = path.join(foldersPath, folder);
+		const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+		// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+		for (const file of commandFiles) {
+			const filePath = path.join(commandsPath, file);
+			const command = require(filePath);
+			if ('data' in command && 'execute' in command) {
+				const commandData = command.data.toJSON();
+				client.commands.set(commandData.name, command);
+				commands.push(commandData);
+			} else {
+				console.warn(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+			}
 		}
 	}
-}
 
-// Construct and prepare an instance of the REST module
-const rest = new REST().setToken(bot_token);
+	// Construct and prepare an instance of the REST module
+	const rest = new REST().setToken(bot_token);
 
-// and deploy your commands!
-(async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
@@ -74,28 +79,26 @@ const rest = new REST().setToken(bot_token);
 		// And of course, make sure you catch and log any errors!
 		console.error(error);
 	}
-})();
-module.exports = (client) => {
-	client.commands = new Collection();
-client.on(Events.InteractionCreate, async (interaction, client) => {
-	if (!interaction.isChatInputCommand()) return;
 
-	const command = interaction.client.commands.get(interaction.commandName);
+	client.on(Events.InteractionCreate, async (interaction) => {
+		if (!interaction.isChatInputCommand()) return;
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+		const command = client.commands.get(interaction.commandName);
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
 		}
-	}
-});
+
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			if (interaction.replied || interaction.deferred) {
+				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+			} else {
+				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			}
+		}
+	});
 }
